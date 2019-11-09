03eda6d9bd4f9177c7bf462e84a6612861767329
FarhanKhanDev <farhan.ahmad.khan.dev@gmail.com>
2019-11-09 22:11:55 +0500

    dummy

--- First.php
+++ First.php
@@ -1,6 +1,6 @@
 "The measuse of intelligence is the ability to change."
 
-- Albert Einteine
+- Albert Einteine.
 
 Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
 
