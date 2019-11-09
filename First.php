e24b10ed2635715b8a40c648e792247e4749819a
FarhanKhanDev <farhan.ahmad.khan.dev@gmail.com>
2019-11-09 23:13:54 +0500

    changes on code_123

--- First.php
+++ First.php
@@ -5,7 +5,7 @@
 Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
 
 - Martin Fowler
-
+need another commit..
 123456
 
 I need to merge these changes into develop branch, that's the reason why i'm writing this so silly.
