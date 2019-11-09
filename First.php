5b478a3f36f66b62314d8de889587219c33fd490
FarhanKhanDev <farhan.ahmad.khan.dev@gmail.com>
2019-11-09 23:13:28 +0500

    changes on code_123

--- First.php
+++ First.php
@@ -1,5 +1,5 @@
 "The measuse of intelligence is the ability to change."
-
+.
 - Albert Einteine.
 
 Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
@@ -8,3 +8,5 @@ Any fool can write code that a computer can understand. Good programmers write c
 
 123456
 
+I need to merge these changes into develop branch, that's the reason why i'm writing this so silly.
+Need to merge these two lines of text.
